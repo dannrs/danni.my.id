d6be89e16922daf3aaf5717b41b8ce2be6ec0933
dannrs <danniramdhani@proton.me>
2024-06-08 22:54:32 +0700

    fix to use custom <Link> component only within mdx files

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,4 +1,5 @@
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
+import { ArrowUpRight } from 'lucide-react'
 
 interface LinkProps {
   href: string
@@ -7,17 +8,37 @@ interface LinkProps {
   className?: string
 }
 
-export default function Link({ className, href, children, "aria-label": ariaLabel }: LinkProps) {
+export default function Link({
+  className,
+  href,
+  children,
+  'aria-label': ariaLabel
+}: LinkProps) {
   const isExternal = href.startsWith('http')
 
   return (
     <a
       href={href}
       target={isExternal ? '_blank' : '_self'}
-      className={cn('underline decoration-muted decoration-2 underline-offset-[3px] duration-200 hover:decoration-muted-3 group-hover:decoration-muted-3 text-foreground', className)}
+      className={cn(
+        'duration-800 m-0 inline-flex bg-gradient-to-b from-accent to-accent bg-[0_100%] bg-no-repeat no-underline transition-[background-size,color] hover:bg-[length:100%_100%] dark:text-[#f1f0ed]',
+        isExternal ? 'bg-[length:100%_2px]' : 'bg-[length:100%_0px]',
+        className
+      )}
       aria-label={ariaLabel}
     >
-      {children}
+      {isExternal ? (
+        <>
+          {children}
+          <ArrowUpRight className='inline h-5 w-5 self-center' />
+        </>
+      ) : (
+        <>
+          <span className='text-accent'>[[</span>
+          {children}
+          <span className='text-accent'>]]</span>
+        </>
+      )}
     </a>
   )
 }
