b16c1772a342eacf68e41d346e0eccaa8e65d2db
dannrs <danniramdhani@proton.me>
2026-06-10 18:43:32 +0700

    chore: switch adapter from vercel to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,13 @@ import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs'
 import icon from 'astro-icon'
 import { unified } from '@astrojs/markdown-remark'
-import vercel from '@astrojs/vercel'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danni.my.id',
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare(),
   integrations: [
     react(),
     tailwind({
